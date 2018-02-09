68d9abb52db997d1d42010efad709ddb9f0b9679
Chris Murphy <_chrism_@live.com>
2018-02-09 10:47:46 +0000

    database cinema created. set up sql file, sql_runner file, and models created for film, customer and ticket

---
+++ models/ticket.rb
@@ -0,0 +1,29 @@
+require_relative ('../db/sql_runner')
+
+class Ticket
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_idx'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      name,
+      funds
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @funds]
+    location = SqlRunner.run( sql, values ).first
+    @id = location['id'].to_i
+  end
+
+
+
+end
